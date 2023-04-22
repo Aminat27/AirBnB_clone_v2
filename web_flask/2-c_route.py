731a7a4f992f313e8adeec78bf4d4446d1f38a13
Aminat27 <adebayoaminat27@gmail.com>
2023-04-22 19:06:40 +0100

    added 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,12 +15,12 @@ def hbnb():
     """return hbnb"""
     return 'HBNB'
 
-@app.route("/c/<text>", strict_slashes=False )
-def c_text():
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_c(text):
     """return string"""
     return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
