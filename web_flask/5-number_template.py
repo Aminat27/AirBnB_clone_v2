d14e6be193c1cd6a8816c6b5ada93d31e409be54
Aminat27 <adebayoaminat27@gmail.com>
2023-04-22 19:45:35 +0100

    added 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -29,13 +29,13 @@ def show_python(text="is cool"):
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/number/<int:n>")
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def show_number_template(n):
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def show_number_html(n):
     """return a html page"""
     return render_template('5-number.html', n=n)
 
